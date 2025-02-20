5744ce0906895f7eaeecb310daecce8d85cc6de7
Byris <byrisfells@gmail.com>
2025-02-20 13:02:09 -0500

    player new changes

--- player.h
+++ player.h
@@ -20,32 +20,15 @@ public:
     Player(std::string name, std::string location, int level, int damage) {
         this->name = name;
         this->location = location;
-        this->level = level;
-        this->damage = damage;
-        this->health = 100;
-        this->maxHealth = 100;
-        this->gold = 0;
-        this->experience = 0;
     }
 
     // Getters
     std::string getName() { return name; }
     std::string getLocation() { return location; }
-    int getLevel() { return level; }
-    int getHealth() { return health; }
-    int getMaxHealth() { return maxHealth; }
-    int getDamage() { return damage; }
-    int getGold() { return gold; }
-    int getExperience() { return experience; }
+
 
     // Setters
     void setLocation(std::string newLocation) { location = newLocation; }
-    void setHealth(int newHealth) { health = newHealth; }
-    void setDamage(int newDamage) { damage = newDamage; }
-    void addGold(int amount) { gold += amount; }
-    void removeGold(int amount) { gold -= amount; }
-    void addExperience(int exp) { experience += exp; }
-    void levelUp() { level++; maxHealth += 20; health = maxHealth; }
 };
 
 #endif
